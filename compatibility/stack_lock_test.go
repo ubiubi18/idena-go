4d6e7cf84c6ffaeb7ade3ee4933f3891c1c05b88
ubiubi18 <ubiubi18@users.noreply.github.com>
2026-07-16 18:10:19 +0200

    Advance compatibility candidate to rc4

--- compatibility/stack_lock_test.go
+++ compatibility/stack_lock_test.go
@@ -11,9 +11,9 @@ import (
 )
 
 const (
-	wantReleaseID             = "idena-mainnet-legacy-compat-2026.07.12-rc3"
-	wantNodeCommit            = "aafb254786ac3c82308550a7a82642019f077d6b"
-	wantRuntimeCommit         = "aafb254786ac3c82308550a7a82642019f077d6b"
+	wantReleaseID             = "idena-mainnet-legacy-compat-2026.07.16-rc4"
+	wantNodeCommit            = "9353030e2066d075ca5ec02241492711edd67ae5"
+	wantRuntimeCommit         = "9353030e2066d075ca5ec02241492711edd67ae5"
 	wantBindingCommit         = "67ba065fdb02aa07cced2a43a261e481ca5b39d9"
 	wantGossipProtocol        = "/idena/gossip/1.1.0"
 	wantMainnetNetwork uint32 = 1
